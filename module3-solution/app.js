9745d2e838f68a56b1e79222c69f164cb9716f0c
UU44BB <xiaolei.wang@ing.com>
2018-06-25 17:22:46 +0200

    md3 solution

---
+++ module3-solution/app.js
@@ -0,0 +1,32 @@
+(function() {
+'use strict';
+
+angular.module('NarrowItDownApp',[])
+       .controller('NarrowItDownController ', NarrowItDownController)
+       .service('MenuSearchService', MenuSearchService)
+       .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json");
+
+NarrowItDownController.$inject=['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var narrow = this;
+  narrow.searchTerm = "";
+  narrow.promise = MenuSearchService.getMatchedMenuItems(narrow.searchTerm);
+  };
+}
+
+MenuSearchService.$inject=['$http','ApiBasePath'];
+function MenuSearchService($http,ApiBasePath) {
+  var service = this;
+  service.getMatchedMenuItems = function(searchTerm) {
+    return $http({
+      method: "GET",
+      url: ApiBasePath
+    }).then(function(result) {
+      var foundItems
+      return foundItems;
+    });
+  };
+}
+
+};
+})();
